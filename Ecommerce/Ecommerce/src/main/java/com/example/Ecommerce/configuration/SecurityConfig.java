a345a5f89117ebdf56f859ad4f8d2419dd77d06c
cuongle2503 <cuongle2503@gmail.com>
2024-10-31 17:04:08 +0700

    update

--- Ecommerce/Ecommerce/src/main/java/com/example/Ecommerce/configuration/SecurityConfig.java
+++ Ecommerce/Ecommerce/src/main/java/com/example/Ecommerce/configuration/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.Ecommerce.configuration;
 
-import com.example.Ecommerce.enums.Role;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
